3fbb5d5848b540c56e4c9e642a962719f7f578bd
ktkrau <ktkrau@gmail.com>
2022-02-18 19:59:15 +0000

    terminado

---
+++ examples/insert.sql
@@ -0,0 +1,8 @@
+-- DML
+/* INSERT INTO table_name (field1, field2, ...,fieldN) VALUES (valor1,valor2, ....,valorN);*/
+
+INSERT INTO users (name, email, phone) VALUES ("Katherine","katherine@gmail.com","+569555-55-55"); /* el id lo genera por defecto*/
+
+INSERT INTO users (name, email, phone) VALUES ("Katherine","katherine.salas@gmail.com","+569555-55-55");
+
+INSERT INTO users (name, email, phone) VALUES ("Frank","frank@gmail.com","+569225-55-55");
